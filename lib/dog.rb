5718d2d1cf6802bc03642124b2e89d7f27c99885
Lessllie Real <rlessllie@yahoo.com>
2019-10-29 01:03:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,13 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end 
   
   def name
     @name
   end
   
-  def initialize(breed)
-    @breed = breed
-  end 
   
   def breed
     @breed
